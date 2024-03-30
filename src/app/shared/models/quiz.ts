86cf033f88a3c500be4e4842eb3cf2700a411db8
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2024-03-30 14:46:04 -0500

    updated html to work on getting quiz to show

--- src/app/shared/models/quiz.ts
+++ src/app/shared/models/quiz.ts
@@ -5,6 +5,7 @@ export class Quiz {
   title: string = '';
   decription: string = '';
   created_date: string = '';
+  // this is where content goes, but not sure how to get the quiz to show here
   total_score_possible: number = 0; // the value here should be less than or = to 100
   // created_by?: User; - not sure if this is needed
   user_id?: number;
