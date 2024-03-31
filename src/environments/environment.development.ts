11a35085155bbe7a8b76d5c3f268cd2558e7eb21
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2024-03-31 17:38:10 -0500

    fixed enviroment, quiz displaying now

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,8 @@
+// export const environment = {
+// 	production: true,
+// 	apiUrl: 'https://www.production.com',
+// };
 export const environment = {
-	production: true,
-	apiUrl: 'https://www.production.com',
+	production: false,
+	apiUrl: 'http://localhost:3000',
 };
