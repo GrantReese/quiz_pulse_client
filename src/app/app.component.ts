4406746630f6ada866eed96971f19b46216c6e09
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2024-03-31 18:54:10 -0500

    main app layout working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 
-
+import { MainLayoutComponent } from './core/components/main-layout/main-layout.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MainLayoutComponent, RouterOutlet], //Should these both be here?
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
