b470d5cf2f9b32c80e2f5fcbc83ffd9d03bc1b6f
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2024-04-01 14:33:54 -0500

    added login and got it working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+
 export const routes: Routes = [
   {
   path: '',
   pathMatch: 'full',
 
 
-  loadComponent: () => import('./features/home/home.component').then((c) => c.HomeComponent),
+  loadComponent: () => import('./features/home/home.component').then((c) => c.HomeComponent)
   },
-
+  {
+  path: 'login',
+  loadComponent: () => import('./features/auth/login/login.component').then((c) => c.LoginComponent)
+  }
 ];
 
 
