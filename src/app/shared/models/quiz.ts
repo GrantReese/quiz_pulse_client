4890ec6939b1cbbcccdbd309480da538a726f47f
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2024-04-09 17:16:10 -0500

    got quizzes displaying

--- src/app/shared/models/quiz.ts
+++ src/app/shared/models/quiz.ts
@@ -5,10 +5,7 @@ export class Quiz {
   title: string = '';
   category: string = '';
   description: string = '';
-  created_date: Date = new Date();
-  quiz_content: string = '';
-  // this is where content goes, but not sure how to get the quiz to show here
-  total_score_possible: number = 0;
+    total_score_possible: number = 0;
   user_id?: number;
   user?: User;
 }
--- src/app/shared/quiz/quiz-list/quiz-list.component.ts
+++ src/app/shared/quiz/quiz-list/quiz-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Quiz } from '../../models/quiz';
-
+import { QuizService } from '../../../core/services/quiz.service';
 @Component({
   selector: 'app-quiz-list',
   standalone: true,
@@ -8,6 +8,20 @@ import { Quiz } from '../../models/quiz';
   templateUrl: './quiz-list.component.html',
   styleUrl: './quiz-list.component.scss'
 })
-export class QuizListComponent {
-@Input({ required: true}) quizzes: Quiz[] = [];
+export class QuizListComponent implements OnInit {
+// @Input({ required: true}) quizzes: Quiz[] = [];
+quizzes: Quiz[] = [];
+
+constructor(private quizService: QuizService) {}
+ngOnInit() {
+  this.quizService.getQuizzes().subscribe({
+    next: (quizzes) => {
+      console.log("This is our pulled data", quizzes);
+      this.quizzes = quizzes;
+    },
+    error: (error) => {
+      console.error(error);
+    },
+  });
+}
 }
