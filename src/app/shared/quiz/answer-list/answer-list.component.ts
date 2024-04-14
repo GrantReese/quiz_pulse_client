89595fcb6d6097d6d4a3fe71275d31609d027833
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2024-04-14 17:33:39 -0500

    Added answer list component

---
+++ src/app/shared/quiz/answer-list/answer-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-answer-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './answer-list.component.html',
+  styleUrl: './answer-list.component.scss'
+})
+export class AnswerListComponent {
+
+}
