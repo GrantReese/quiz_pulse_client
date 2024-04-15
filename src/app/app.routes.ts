3b1e2c1aa9afd1c98926bb699b9235837a97af2f
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2024-04-15 15:56:10 -0500

    worked on question list component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -26,6 +26,8 @@ export const routes: Routes = [
     path: 'questions/new',
     loadComponent: () => import('./features/questions/questions-new/questions-new.component').then((c) => c.QuestionsNewComponent)
   }
+  path: 'question-list',
+  loadComponent: () => import('./features/questions/question-list/question-list.component').then((c) => c.QuestionListComponent)
 ];
 
 
---
+++ src/app/features/questions/question-list/question-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { QuestionService } from '../../../core/services/question.service';
+import { Question } from '../../../shared/models/question';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-question-list',
+  standalone: true,
+  imports: [QuestionListComponent],
+  templateUrl: './question-list.component.html',
+  styleUrl: './question-list.component.scss'
+})
+export class QuestionListComponent implements OnInit {
+  constructor(private questionService: QuestionService) { }
+
+  ngOnInit(): void {
+    this.questionService.getQuestions().subscribe({
+      next: (questions) => {
+        console.log(questions);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+}
